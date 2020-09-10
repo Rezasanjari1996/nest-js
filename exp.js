78ce83945ed8da6e489d2e445ee8c038723199e1
reza sanajri <sanjarireza1996@gmail.com>
2020-09-10 11:37:49 +0430

    to write methods get and put and delet and post

--- exp.js
+++ exp.js
@@ -1,14 +1,41 @@
 const express=require("express");
 const app=express();
-app.get('/root/:a/:b',(req,res)=>{
+app.get('/get/:a/:b',(req,res)=>{
   console.log(req.params) ;
   const {a, b} = req.params;
     let sum = 0 ;
     sum = parseInt(a) + parseInt(b);
     console.log(sum);
     res.send(sum.toString());
-  
- 
 });
+
+app.post('/post/:a/:b',(req,res)=>{
+    console.log(req.params) ;
+    const {a, b} = req.params;
+      let sum = 0 ;
+      sum = parseInt(a) * parseInt(b);
+      console.log(sum);
+      res.send(sum.toString());
+  });
+
+  app.put('/put/:a/:b',(req,res)=>{
+    console.log(req.params) ;
+    const {a, b} = req.params;
+      let sum = 0 ;
+      sum = parseInt(a) / parseInt(b);
+      console.log(sum);
+      res.send(sum.toString());
+  });
+
+  app.delete('/delete/:a/:b',(req,res)=>{
+    console.log(req.params) ;
+    const {a, b} = req.params;
+      let sum = 0 ;
+      sum = parseInt(a) - parseInt(b);
+      console.log(sum);
+      res.send(sum.toString());
+  });
+  
+  
 
 app.listen(3000,()=>console.log('server is runnig on localhost:3000'));
